83a484a90fc3f28636b06d16b33076e110a358ca
sewkyz1 <134824455+sewkyz1@users.noreply.github.com>
2024-07-23 17:43:50 +0800

    Delete external/AR/ltr directory

--- external/AR/ltr/actors/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base_actor import BaseActor
-from .bbreg import AtomActor
-from .tracking import DiMPActor, KYSActor
-from ltr.actors.ar_actors.SEcm import SEcm_Actor
-from ltr.actors.ar_actors.SEc import SEc_Actor
-from ltr.actors.ar_actors.SEm import SEm_Actor
